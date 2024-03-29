59a29fc5ba066f9d25124b22af46d1cc53445f96
temetNoscee <piatesla09@gmail.com>
2024-03-29 15:17:15 +0300

    this is better

--- future/src/main/java/com/future/furniture/FurnitureController.java
+++ future/src/main/java/com/future/furniture/FurnitureController.java
@@ -2,11 +2,13 @@ package com.future.furniture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/api")
 
 public class FurnitureController {
     private final FurnitureService furnitureService;
