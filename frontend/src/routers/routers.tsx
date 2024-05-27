d2975895855a74f7ffba10cb731e6344a377a69e
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2024-05-27 18:22:50 +0300

    implement /signup and /login route

--- frontend/src/routers/routers.tsx
+++ frontend/src/routers/routers.tsx
@@ -1,4 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  ActionFunctionArgs,
+  createBrowserRouter,
+  redirect,
+} from "react-router-dom";
 
 import AddProduct from "../admin/addProdut";
 import AdminNav from "../admin/adminNav";
@@ -10,6 +14,8 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import ProductDetails from "../pages/ProductDetails";
 import Shop from "../pages/Shop";
+import { api } from "../api";
+import axios from "axios";
 
 export const routes = createBrowserRouter([
   {
@@ -24,7 +30,47 @@ export const routes = createBrowserRouter([
       { path: "dashboard/add-product", element: <AddProduct /> },
       { path: "dashboard/answer", element: <Questions /> },
       { path: "dashboard/quantity", element: <Quantity /> },
-      { path: "login", element: <Login /> },
+      {
+        path: "login",
+        element: <Login />,
+        action: async ({ request }: ActionFunctionArgs) => {
+          const form = await request.formData();
+          const _action = form.get("_action") as "login" | "signup";
+          if (_action === "login") {
+            const email = form.get("email") as string;
+            const password = form.get("password") as string;
+            const { data: token } = await axios.post(
+              "http://localhost:8080/api/v1/user/login",
+              null,
+              {
+                params: {
+                  email,
+                  password,
+                },
+              }
+            );
+            localStorage.setItem("token", token);
+            return redirect("/");
+          } else {
+            const email = form.get("email") as string;
+            const password = form.get("password") as string;
+            const username = form.get("username") as string;
+            const { data: token } = await axios.post(
+              "http://localhost:8080/api/v1/user/signup",
+              null,
+              {
+                params: {
+                  email,
+                  password,
+                  username,
+                },
+              }
+            );
+            localStorage.setItem("token", token);
+            return redirect("/");
+          }
+        },
+      },
     ],
   },
 ]);
