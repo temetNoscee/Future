c3627787f1c89b8d793eb5a1b4debef8a982dae0
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2024-05-27 21:48:12 +0300

    i'm tired of having to re-register.

---
+++ backend/src/main/java/com/future/user/UserConfig.java
@@ -0,0 +1,23 @@
+package com.future.user;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserConfig {
+    @Bean
+    CommandLineRunner runner(UserRepository userRepository, TokenRepository tokenRepository) {
+        return args -> {
+            User user = new User();
+            user.setUsername("admin");
+            user.setPassword("123");
+            user.setEmail("admin@email.com");
+            userRepository.save(user);
+            Token token = new Token();
+            token.setUser(user);
+            token.setToken("admin-token");
+            tokenRepository.save(token);
+        };
+    }
+}
